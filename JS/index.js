b9b75551e22d11f9ed4d4436306cd7fcd8460c57
bowenxi <95603540+bowenxi96@users.noreply.github.com>
2022-05-28 00:14:48 -0400

    final

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,31 @@
-console.log("Your index.js file is loaded correctly!");
+/*const scrollUp = document.querySelector('.scrollUp');
+
+window.addEventListener('scroll', () => {
+    if(window.pageXOffset > 100){
+        scrollUp.classList.add("active");
+    }else{
+        scrollUp.classList.remove("active");
+    }
+});
+*/
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 900){
+            $('#topBtn').fadeIn();
+        } else{
+
+            $('#topBtn').fadeOut();
+
+
+        }
+    });
+
+$("#topBtn").click(function(){
+
+    $('html ,body').animate({scrollTop : 0},800)
+})
+
+});
 
